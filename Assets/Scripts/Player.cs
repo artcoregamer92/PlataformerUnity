d5091863014865d75d2765ba834957b913d92cae
LYNDA EVELYN (SCATHLYN) <129218205+artcoregamer92@users.noreply.github.com>
2025-05-06 18:44:02 -0500

    Adelanto plataformer base

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float inputH;
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float fuerzaSalto;
+
+    [Header("Sistema de combate")]
+    [SerializeField] private Transform puntoAtaque;
+    [SerializeField] private float radioAtaque;
+    [SerializeField] private float danhoAtaque;
+    [SerializeField] private LayerMask queEsDanhable;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimiento();
+
+        Jump();
+
+        LanzarAtaque();
+    }
+
+    private void LanzarAtaque()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Attack");
+        }
+    }
+
+    //Se ejecuta desde evento de animación.
+    private void Attack()
+    {
+        //Lanzar trigger instantaneo.
+        Collider2D[] collidersTocados = Physics2D.OverlapCircleAll(puntoAtaque.position, radioAtaque, queEsDanhable);
+        foreach (Collider2D item in collidersTocados)
+        {
+            SistemaVidas sistemaVidas = item.gameObject.GetComponent<SistemaVidas>();
+            sistemaVidas.RecibirDanho(danhoAtaque);
+        }
+
+    }
+
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            anim.SetTrigger("Jump");
+        }
+    }
+
+    private void Movimiento()
+    {
+        inputH = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(inputH * velocidadMovimiento, rb.velocity.y);
+        if (inputH != 0) //Hay movimiento
+        {
+            anim.SetBool("Running", true);
+            if (inputH > 0) //Dcha
+            {
+                transform.eulerAngles = Vector3.zero;
+            }
+            else //Izq
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
+        else //inputH = 0
+        {
+            anim.SetBool("Running", false);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(puntoAtaque.position, radioAtaque);
+    }
+}
